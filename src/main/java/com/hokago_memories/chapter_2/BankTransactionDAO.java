f9783b362fef72c92708e27d9bbd6add8084d41c
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-09-02 23:30:23 +0900

    정보 응집 예제 작성

---
+++ src/main/java/com/hokago_memories/chapter_2/BankTransactionDAO.java
@@ -0,0 +1,27 @@
+package com.hokago_memories.chapter_2;
+
+import java.time.LocalDate;
+
+// BankTransaction의 CRUD 클래스
+public class BankTransactionDAO {
+     public BankTransaction create(final LocalDate date, final double amount, final String description) {
+         // ...
+         throw new UnsupportedOperationException();
+     }
+
+     public BankTransaction read(final long id) {
+         // ...
+         throw new UnsupportedOperationException();
+     }
+
+     public BankTransaction update(final long id) {
+         // ...
+         throw new UnsupportedOperationException();
+     }
+
+     public BankTransaction delete(final BankTransaction BankTransaction) {
+         // ...
+         throw new UnsupportedOperationException();
+     }
+
+}
