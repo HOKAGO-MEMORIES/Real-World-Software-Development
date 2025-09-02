97b58dcf184f1fbe3689a365c60d137e6343c580
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-09-02 18:34:28 +0900

    CSV 파싱 로직 분리

---
+++ src/main/java/com/hokago_memories/chapter_2/BankStatementCSVParser.java
@@ -0,0 +1,33 @@
+package com.hokago_memories.chapter_2;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+// CSV 파싱 로직을 BankStatementCSVParser에 분리
+public class BankStatementCSVParser {
+    private static final DateTimeFormatter DATE_PATTERN
+            = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+
+    // BankTransaction 객체를 생성하는 클래스 정의
+
+    private BankTransaction parseFromCSV(final String line) {
+        final String[] columns = line.split(",");
+
+        final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+        final double amount = Double.parseDouble(columns[1]);
+        final String description = columns[2];
+
+        return new BankTransaction(date, amount, description);
+    }
+
+    public List<BankTransaction> parseLinesFromCSV(final List<String> lines) {
+        final List<BankTransaction> bankTransactions = new ArrayList<>();
+        for (final String line : lines) {
+            bankTransactions.add(parseFromCSV(line));
+        }
+        return bankTransactions;
+    }
+}
