26d582b94328f5182f198073b1a241625a96afcb
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-09-02 21:33:57 +0900

    계산 연산 추출

---
+++ src/main/java/com/hokago_memories/chapter_2/BankStatementProcessor.java
@@ -0,0 +1,41 @@
+package com.hokago_memories.chapter_2;
+
+import java.time.Month;
+import java.util.List;
+
+// 계산 연산을 추출하여 응집도 개선
+public class BankStatementProcessor {
+    private final List<BankTransaction> bankTransactions;
+
+    public BankStatementProcessor(final List<BankTransaction> bankTransactions) {
+        this.bankTransactions = bankTransactions;
+    }
+
+    public double calculateAmount() {
+        double total = 0;
+        for (final BankTransaction bankTransaction: bankTransactions) {
+            total += bankTransaction.getAmount();
+        }
+        return total;
+    }
+
+    public double calculateTotalInMonth(final Month month) {
+        double total = 0;
+        for (final BankTransaction bankTransaction: bankTransactions) {
+            if (bankTransaction.getDate().getMonth() == month) {
+                total += bankTransaction.getAmount();
+            }
+        }
+        return total;
+    }
+
+    public double calculateTotalForCategory(final String category) {
+        double total = 0;
+        for (final BankTransaction bankTransaction: bankTransactions) {
+            if (bankTransaction.getDescription().equals(category)) {
+                total += bankTransaction.getAmount();
+            }
+        }
+        return total;
+    }
+}
