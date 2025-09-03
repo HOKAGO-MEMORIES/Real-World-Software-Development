817ddce68ed20f773d64c39bbc1c12d819f678e9
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-09-03 16:59:31 +0900

    개방/폐쇄 원칙 적용

--- src/main/java/com/hokago_memories/chapter_3/BankStatementProcessor.java
+++ src/main/java/com/hokago_memories/chapter_3/BankStatementProcessor.java
@@ -1,6 +1,7 @@
 package com.hokago_memories.chapter_3;
 
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BankStatementProcessor {
@@ -36,5 +37,41 @@ public class BankStatementProcessor {
             }
         }
         return total;
+    }
+
+    /* 요구 사항에 맞추기 위해 해당 방식으로 코드를 작성하면 코드 중복 문제가 발생
+    public List<BankTransaction> findTransactionsGreaterThanEqual(final int amount) {
+        final List<BankTransaction> result = new ArrayList<>();
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            if (bankTransaction.getAmount() >= amount) {
+                result.add(bankTransaction);
+            }
+        }
+        return result;
+    }
+
+
+    public List<BankTransaction> findTransactionsInMonth(final Month month) {
+        final List<BankTransaction> result = new ArrayList<>();
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            if (bankTransaction.getDate().getMonth() == month) {
+                result.add(bankTransaction);
+            }
+        }
+        return result;
+    }
+    */
+
+
+    // 개방/폐쇄 원칙 적용 -> 메서드의 변경 없이도 확장성은 개방됨
+    // 요구사항에 맞춰서 계속 똑같은 코드를 중복해서 작성할 필요 없음
+    public List<BankTransaction> findTransactions(final BankTransactionFilter bankTransactionFilter) {
+        final List<BankTransaction> result = new ArrayList<>();
+        for (final BankTransaction bankTransaction : bankTransactions) {
+            if (bankTransactionFilter.test(bankTransaction)) {
+                result.add(bankTransaction);
+            }
+        }
+        return result;
     }
 }
