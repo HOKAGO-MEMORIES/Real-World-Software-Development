314ed19af6ed785b7dd031a9179d889dfff7e5d9
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-09-04 01:32:03 +0900

    리포트 임포트

--- src/main/java/com/hokago_memories/chapter_4/ReportImporter.java
+++ src/main/java/com/hokago_memories/chapter_4/ReportImporter.java
@@ -1,4 +1,22 @@
 package com.hokago_memories.chapter_4;
 
+import static com.hokago_memories.chapter_4.Attributes.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
 class ReportImporter implements Importer {
+    private static final String NAME_PREFIX = "Patient: ";
+
+    @Override
+    public Document importFile(final File file) throws IOException {
+        final TextFile textFile = new TextFile(file);
+        textFile.addLineSuffix(NAME_PREFIX, PATIENT);
+        textFile.addLines(2, line -> false, BODY);
+
+        final Map<String, String> attributes = textFile.getAttributes();
+        attributes.put(TYPE, "REPORT");
+        return new Document(attributes);
+    }
 }
