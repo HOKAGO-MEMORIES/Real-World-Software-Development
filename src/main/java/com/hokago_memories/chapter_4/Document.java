959d297d6257894e910012ff6ba0d5914cb11c87
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-09-03 23:41:48 +0900

    이미지를 임포트하는 클래스 작성

--- src/main/java/com/hokago_memories/chapter_4/Document.java
+++ src/main/java/com/hokago_memories/chapter_4/Document.java
@@ -5,6 +5,7 @@ import java.util.Map;
 public class Document {
     private final Map<String, String> attributes;
 
+    // 패키지 영역으로 생성자를 제공하고, 시스템이 위치한 패키지에만 접근 권한을 주기 위해 public 사용 안함 (default 사용)
     Document(Map<String, String> attributes) {
         this.attributes = attributes;
     }
