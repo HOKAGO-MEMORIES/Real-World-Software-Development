8666485e1ca26408f0c62aec504b86b1aeb11c51
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-09-04 00:18:47 +0900

    임포터 검증 메서드 작성

--- src/main/java/com/hokago_memories/chapter_4/DocumentManagementSystem.java
+++ src/main/java/com/hokago_memories/chapter_4/DocumentManagementSystem.java
@@ -1,9 +1,15 @@
 package com.hokago_memories.chapter_4;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DocumentManagementSystem {
+    private final List<Document> documents = new ArrayList<>();
     private final Map<String, Importer> extensionToImporter = new HashMap<>();
 
     // Importer 클래스로 파일을 임포트 하기 위해 클래스 등록
@@ -11,5 +17,32 @@ public class DocumentManagementSystem {
         extensionToImporter.put("letter", new LetterImporter());
         extensionToImporter.put("report", new ReportImporter());
         extensionToImporter.put("jpg", new ImageImporter());
+    }
+
+    // Importer를 실행하기 전에 검증하는 메서드
+    // 하위 형식에서(Importer 안에서) 선행 조건을 더하지 못하기에 이곳에서 조건을 검 (리스코프 치환 원칙)
+    public void importFile(final String path) throws IOException {
+        final File file = new File(path);
+        if (!file.exists()) {
+            throw new FileNotFoundException(path);
+        }
+
+        final int separatorIndex = path.lastIndexOf('.');
+        if (separatorIndex != -1) {
+            if (separatorIndex == path.length()) {
+                throw new UnknownFileTypeException("No extension found For file: " + path);
+            }
+
+            final String extension = path.substring(separatorIndex + 1);
+            final Importer importer = extensionToImporter.get(extension);
+            if (importer == null) {
+                throw new UnknownFileTypeException("For file: " + path);
+            }
+
+            final Document document = importer.importFile(file);
+            documents.add(document);
+        } else {
+            throw new UnknownFileTypeException("No extension found For file: " + path);
+        }
     }
 }
---
+++ src/main/java/com/hokago_memories/chapter_4/UnknownFileTypeException.java
@@ -0,0 +1,7 @@
+package com.hokago_memories.chapter_4;
+
+public class UnknownFileTypeException extends RuntimeException {
+    public UnknownFileTypeException(final String message) {
+        super(message);
+    }
+}
