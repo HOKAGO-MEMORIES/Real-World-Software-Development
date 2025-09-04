ed0f6d810f8c15f7f67577e93ab29dc1746134cb
HOKAGO-MEMORIES <minchotejava@gmail.com>
2025-09-04 22:10:19 +0900

    생성된 빌더에 맞춰 비즈니스 규칙 엔진 리팩토링

--- src/main/java/com/hokago_memories/chapter_5/BusinessRuleEngine.java
+++ src/main/java/com/hokago_memories/chapter_5/BusinessRuleEngine.java
@@ -4,14 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BusinessRuleEngine {
-    private final List<Action> actions;
+    // private final List<Action> actions;
+    private final List<Rule> rules;
     private final Facts facts;
 
     public BusinessRuleEngine(final Facts facts) {
         this.facts = facts;
-        this.actions = new ArrayList<>();
+        this.rules = new ArrayList<>();
+        // this.actions = new ArrayList<>();
+
     }
 
+    /* 이전 코드
     public void addAction(final Action action) {
         this.actions.add(action);
     }
@@ -22,5 +26,16 @@ public class BusinessRuleEngine {
 
     public void run() {
         this.actions.forEach(action -> action.perform(facts));
+    }
+    */
+
+    // 비즈니스 규칙 엔진이 액션 대신 규칙을 지원하도록 리팩토링
+
+    public void addRule(final Rule rule) {
+        this.rules.add(rule);
+    }
+
+    public void run() {
+        this.rules.forEach(rule -> rule.perform(facts));
     }
 }
